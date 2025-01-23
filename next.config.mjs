3c6bbc6a0a51d7c383c3cb1f57a3ef38d20d8f6d
jubayerahmmad <zubayersami510@gmail.com>
2025-01-23 11:20:01 +0600

    re-deploy

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
+  env: {
+    KINDE_SITE_URL:
+      process.env.KINDE_SITE_URL ?? `https://simple-next-app-gjrt.vercel.app`,
+    KINDE_POST_LOGOUT_REDIRECT_URL:
+      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ??
+      `https://simple-next-app-gjrt.vercel.app`,
+    KINDE_POST_LOGIN_REDIRECT_URL:
+      process.env.KINDE_POST_LOGIN_REDIRECT_URL ??
+      `https://simple-next-app-gjrt.vercel.app/profile`,
+  },
 };
 
 export default nextConfig;
